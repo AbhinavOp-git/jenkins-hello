31c866116278fffbcc8a1c5ccefaf425ea750462
AbhinavOp-git <75534709+AbhinavOp-git@users.noreply.github.com>
2025-04-18 18:12:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,10 @@
-print(" 🔥 Hello from my Docker + Jemkins Python App!")
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return "Hello from Flask inside Docker + Jenkins 🚀"
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=8000)
+
